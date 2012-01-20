9c7906513c3c11c49238959ee490ecf676386d21
Arash Badie Modiri <arashbm@gmail.com>
2012-01-20 04:16:49 +0330

    Added a bit of Arashish things

--- hello.rb
+++ hello.rb
@@ -5,13 +5,34 @@ Camping.goes :Hello
 module Hello::Controllers
   class Index < R '/'
     def get
+      @time=Time.now
+      day=@time.day
+      @ordinalized_day= if (11..13).include? day
+	"#{day}th"
+      else
+	case day % 10
+	  when 1; "#{day}st"
+          when 2; "#{day}nd"
+          when 3; "#{day}rd"
+          else    "#{day}th"
+	end
+      end
+
       render :hello
     end
   end
 end
 
 module Hello::Views
+  def layout
+    html do
+      head do
+	title { "Arash goes camping!" }
+      end
+      body { self << yield }
+    end
+  end
   def hello
-    p "Hello, Camping!"
+    p "It's #{@time.strftime("%l %p")}, #{@time.strftime("%B")} the #{@ordinalized_day}, and Arash is camping!!"
   end
 end
